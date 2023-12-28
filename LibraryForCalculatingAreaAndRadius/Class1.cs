97076deb20f65fe40b1b74e6e43f10c512a23314
Deadlord <ali.gasanow9@gmail.com>
2023-12-28 20:07:49 +0400

    Добавьте файлы проекта.

---
+++ LibraryForCalculatingAreaAndRadius/Class1.cs
@@ -0,0 +1,60 @@
+﻿namespace LibraryForCalculatingAreaAndRadius
+{
+    
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+
+    public class Circle : IShape
+    {
+        private readonly double radius;
+
+        public Circle(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentException("Радиус не может быть отрицательным.", nameof(radius));
+
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+
+    public class Triangle : IShape
+    {
+        private readonly double sideA;
+        private readonly double sideB;
+        private readonly double sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Длины сторон треугольника должны быть положительными числами.");
+
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double s = (sideA + sideB + sideC) / 2;
+            double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+            return area;
+        }
+
+        public bool IsRightTriangle()
+        {
+            
+            return Math.Pow(sideA, 2) + Math.Pow(sideB, 2) == Math.Pow(sideC, 2) ||
+                   Math.Pow(sideB, 2) + Math.Pow(sideC, 2) == Math.Pow(sideA, 2) ||
+                   Math.Pow(sideA, 2) + Math.Pow(sideC, 2) == Math.Pow(sideB, 2);
+        }
+    }
+}
---
+++ TestLibrary/Program.cs
@@ -0,0 +1,62 @@
+﻿using LibraryForCalculatingAreaAndRadius;
+
+namespace TestLibrary
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool continueCircle = true;
+
+            do
+            {
+                Console.Write("Введите площадь круга: ");
+                string radiusString = Console.ReadLine()!;
+
+                if (double.TryParse(radiusString, out double radius))
+                {
+                    Console.WriteLine($"Площадь круга: {new Circle(radius).CalculateArea()}");
+                    continueCircle = false; 
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод радиуса. Пожалуйста, введите число.");
+                }
+            } while (continueCircle);
+
+          
+            bool continueTriangle = true;
+
+            do
+            {
+               
+                Console.Write("Введите длину стороны A треугольника: ");
+                string sideAString = Console.ReadLine()!;
+
+                Console.Write("Введите длину стороны B треугольника: ");
+                string sideBString = Console.ReadLine()!;
+
+                Console.Write("Введите длину стороны C треугольника: ");
+                string sideCString = Console.ReadLine()!;
+
+            
+                if (double.TryParse(sideAString, out double sideA) &&
+                    double.TryParse(sideBString, out double sideB) &&
+                    double.TryParse(sideCString, out double sideC))
+                {
+                    IShape triangle = new Triangle(sideA, sideB, sideC);
+                    Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea()}");
+                    Console.WriteLine($"Прямоугольный треугольник: {((Triangle)triangle).IsRightTriangle()}");
+
+                    
+                    continueTriangle = false; 
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод сторон треугольника. Пожалуйста, введите числа.");
+                }
+            } while (continueTriangle);
+
+        }
+    }
+}
